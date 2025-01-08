d5ba60ba2c0d0a01efde6a178a954bfde620cead
Justin Paulson <justinapaulson@gmail.com>
2025-01-08 13:10:30 -0600

    2015 Day 7 Cleanup

--- 2015/7/solution.rb
+++ 2015/7/solution.rb
@@ -1,3 +1,4 @@
+ARGV[0] ||= "input"
 # &	Bitwise AND
 # |	Bitwise OR
 # ^	Bitwise XOR
@@ -5,21 +6,16 @@
 # <<	Bitwise left shift
 # >> Bitwise right shift
 
-lines = File.readlines("input")
+lines = File.readlines(ARGV[0])
 
 wires = {}
 
 lines.each do |line|
   code, wire = line.chomp.split(" -> ")
-  code = code.gsub("NOT ", "~")
-  code = code.gsub(" AND ", "&")
-  code = code.gsub(" OR ", "|")
-  code = code.gsub(" LSHIFT ", "<<")
-  code = code.gsub(" RSHIFT ", ">>")
+  code = code.gsub("NOT ", "~").gsub(" AND ", "&").gsub(" OR ", "|")
+    .gsub(" LSHIFT ", "<<").gsub(" RSHIFT ", ">>")
   wires[wire] = code
 end
-
-puts wires.to_s
 
 wire_values = {}
 
@@ -31,19 +27,13 @@ while wire_values.length != wires.length
         ready = false unless wire_values[var]
       end
       next unless ready
-      puts code if wire == "t"
       code.scan(/[a-z]+/){|rep| code = code.gsub(rep, wire_values[rep].to_s)}
-      puts code if wire == "t"
       wire_values[wire] = eval(code)
     rescue TypeError => e
-      puts "we got here dammit" if wire == "t"
       raise e if wire == "t"
     end
   end
-  puts wire_values.to_s
 end
-
-puts wire_values.to_s
 
 puts wire_values["a"]
 
@@ -59,18 +49,12 @@ while wire_values.length != wires.length
         ready = false unless wire_values[var]
       end
       next unless ready
-      puts code if wire == "t"
       code.scan(/[a-z]+/){|rep| code = code.gsub(rep, wire_values[rep].to_s)}
-      puts code if wire == "t"
       wire_values[wire] = eval(code)
     rescue TypeError => e
-      puts "we got here dammit" if wire == "t"
       raise e if wire == "t"
     end
   end
-  puts wire_values.to_s
 end
-
-puts wire_values.to_s
 
 puts wire_values["a"]
