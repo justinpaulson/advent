e7278b8550ba29cb95c4b7be2f65b84778e469ca
Justin Paulson <justinapaulson@gmail.com>
2025-01-08 13:31:34 -0600

    2015 Day 9 Redo

--- 2015/9/solution.rb
+++ 2015/9/solution.rb
@@ -1,5 +1,30 @@
-lines = File.readlines("input")
+ARGV[0] ||= "input"
+lines = IO.readlines(ARGV[0]).map(&:chomp)
 
-ans=0
+distances = {}
 
-puts ans
+lines.each do |line|
+  cities, distance = line.split(" = ")
+  distance = distance.to_i
+  cities = cities.split(" to ")
+  distances[cities[0]] ||= {}
+  distances[cities[0]][cities[1]] = distance
+  distances[cities[1]] ||= {}
+  distances[cities[1]][cities[0]] = distance
+end
+
+longest = 0
+shortest = 99999999
+distances.keys.permutation.each do |perm|
+  distance = 0
+  perm.each_with_index do |city, i|
+    if i < perm.length - 1
+      distance += distances[city][perm[i+1]]
+    end
+  end
+  longest = distance if distance > longest
+  shortest = distance if distance < shortest
+end
+
+puts shortest
+puts longest
