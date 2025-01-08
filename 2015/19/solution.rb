661b0c31be9afac349f12b955775bd918da77e9f
Justin Paulson <justinapaulson@gmail.com>
2025-01-08 14:36:17 -0600

    2015 Day 19 cleanup

--- 2015/19/solution.rb
+++ 2015/19/solution.rb
@@ -1,8 +1,8 @@
 require 'set'
+ARGV[0] = "input"
 
-molecule = "CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr"
-
-@lines = IO.readlines("input").map{|l| l.chomp.split(" => ")}
+@lines, molecule = IO.read(ARGV[0]).split("\n\n")
+@lines = @lines.split("\n").map{|l| l.chomp.split(" => ")}
 
 moles = Set.new
 
@@ -17,49 +17,12 @@ puts moles.count
 
 new_mole = "e"
 
-require '../../grid.rb'
-
-def is_goal? point
-  point == "e"
-end
-
-def get_neighbors point
-  neighbors = []
-
-  @lines.each do |(to, from)|
-    point.scan(/#{from}/) do |c|
-      neighbors << point[0..Regexp.last_match.offset(0).first-1] + to + point[Regexp.last_match.offset(0).first+from.length..-1]
-    end
-  end
-
-  neighbors
-end
-
-def score_for_neighbor point, neighbor
-  1
-end
-
-def h point
-  total = point.length
-  total += 33 * point.scan(/HF|NAl|OMg/).length
-  total += 10 * point.scan(/BF|TiMg|CrnFYFAr|CRnMgAr|HP|NRnFAr|OTi|PMg/).length
-  total += 8 * point.scan(/SiRnFAr|SiRnMgAr|SiTh|CaCa|PB|PRnFAr|CaSi/).length
-  total
-end
-
-def print_a_star curr_point, lowest, neighbors, set, from, g, f
-  puts "#{lowest} => #{curr_point}"
-end
-
-# puts a_star(molecule)
-
 count = 0
 iters = 0
 while molecule != 'e'
   @lines.each do |(to, from)|
     iters += 1
     new_molecule = molecule.sub(from, to)
-    puts new_molecule
     if (new_molecule) != molecule
       molecule = new_molecule
       count += 1
@@ -67,5 +30,4 @@ while molecule != 'e'
   end
 end
 
-puts iters
 puts count
