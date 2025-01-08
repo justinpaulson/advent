ea0c54b86557d12ca20a2d3eed6b514adc018527
Justin Paulson <justinapaulson@gmail.com>
2025-01-08 13:12:24 -0600

    2015 Day 8 Cleanup

--- 2015/8/solution.rb
+++ 2015/8/solution.rb
@@ -1,11 +1,12 @@
-lines = File.readlines("input")
+ARGV[0] ||= "input"
+lines = File.readlines(ARGV[0])
 
 code_total = 0
 memory_total = 0
 encode_total = 0
 
 def mem_total line
-  line = line[1..-2] #remove " ""
+  line = line[1..-2]
   line = line.gsub("\\\"", "'")
   line = line.gsub("\\\\", ".")
   line = line.gsub(/\\x[a-f0-9]{2}/, ".")
@@ -13,11 +14,9 @@ def mem_total line
 end
 
 def enc_total line
-  puts line
-  line = line.gsub("\\", "\\\\\\\\") # so weird
+  line = line.gsub("\\", "\\\\\\\\")
   line = line.gsub("\"", "\\\"")
   line = "\"" + line + "\""
-  puts line
   line.length
 end
 
@@ -27,9 +26,6 @@ lines.each do |line|
   memory_total += mem_total line
   encode_total += enc_total line
 end
-
-puts code_total
-puts memory_total
 
 ans=code_total - memory_total
 
