fe1ad7ace656179991b3fd0a072c726c2878c6ec
Justin Paulson <justinapaulson@gmail.com>
2025-01-08 22:24:08 -0600

    2015 Day 20 Cleanup

--- 2015/20/solution.rb
+++ 2015/20/solution.rb
@@ -1,7 +1,10 @@
-input = 33100000
+ARGV[0] ||= "input"
+input = File.read(ARGV[0]).to_i
 
 # get all divisors of a number
+@divisors = {}
 def divisors_of(number)
+  return @divisors[number] if @divisors[number]
   divisors = []
   (1..Math.sqrt(number)).each do |i|
     if number % i == 0
@@ -9,24 +12,22 @@ def divisors_of(number)
       divisors << number / i
     end
   end
-  divisors.uniq
+  @divisors[number]=divisors.uniq
 end
 
-# part 1
-# house = 0
-# total = 0
-# while total < input
-#   house += 1
-#   total = divisors_of(house).inject(:+) * 10
-# end
-
-# puts house
-
-# part 2
+# slow
 house = 0
 total = 0
 while total < input
-  puts house if house % 10000 == 0
+  house += 1
+  total = divisors_of(house).inject(:+) * 10
+end
+
+puts house
+
+house = 0
+total = 0
+while total < input
   house += 1
   total = divisors_of(house).select { |d| house / d <= 50 }.inject(:+) * 11
 end
